c601334d918af49e918879cd3bf784d6689db1e8
nurulislamrimon <nurulislamrimon@gmail.com>
2024-04-18 15:39:18 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,47 @@
 "use client";
-import EditorJs from "@editorjs/editorjs";
+
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import { useEffect } from "react";
 
 export default function Home() {
-  const editor = new EditorJs({
+  const editor = new EditorJS({
     holder: "editorjs",
-    autofocus: true,
+    // inlineToolbar: ["link", "marker", "bold", "italic"],
+    tools: {
+      header: {
+        class: Header,
+        inlineToolbar: true,
+        shortcut: "CMD+SHIFT+H",
+        config: {
+          placeholder: "Header",
+        },
+      },
+    },
+    placeholder: "Let`s write an awesome story!",
+    // onChange: (api, event) => {
+    //   console.log("Now I know that Editor's content changed!", event, api);
+    // },
   });
-  console.log(editor);
+
+  const handleSubmit = () => {
+    editor
+      .save()
+      .then((outputData) => {
+        console.log("Article data: ", outputData);
+      })
+      .catch((error) => {
+        console.log("Saving failed: ", error);
+      });
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hello
       <div id="editorjs"></div>
+      <button onClick={handleSubmit} type="submit">
+        Submit
+      </button>
     </main>
   );
 }
